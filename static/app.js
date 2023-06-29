4e3d6a18339335b2e0d4b9c4b5c04dced364a297
Andrew Skorupa <andrewskorupa@yahoo.com>
2023-06-28 22:01:39 -0400

    finished up homework

--- static/app.js
+++ static/app.js
@@ -4,42 +4,42 @@ console.log("this is a test")
 const testing_data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 d3.json(testing_data).then(function(data) {
-    console.log(data);
+    //console.log(data);
 });
 
 function buildingTheCharts(sample) {
     console.log("building the charts");
-    dj.json(testing_data).then(function(data)) {
-        console.log("building the charts")
-        console.log(data);
+    d3.json(testing_data).then(function(data) {
+        //console.log("building the charts")
+        //console.log(data);
 
 
-        let samples = data.samples
-        console.log(samples)
+        let moduleSamples = data.samples
+        //console.log(moduleSamples)
 
-        let sampArray = samples.filter(sampleObject => sampleObject.id == sample);
-        console.log(sampArray);
+        let sampArray = moduleSamples.filter(sampleObject => sampleObject.id == sample);
+        //console.log(sampArray);
 
         let sampResult = sampArray[0];
         console.log(sampResult);
 
 
-        let samples = sampResult.samples;
-        console.log(samplesinStudy);
+        let samplesData = sampResult.sample_values;
+        //console.log(samplesData);
 
         let otu_ids = sampResult.otu_ids;
-        console.log(otu_ids);
+        //console.log(otu_ids);
 
         let otu_labels = sampResult.otu_labels;
-        console.log(otu_labels);
+        //console.log(otu_labels);
 
 
         let TraceBubbleChart = {
             x: otu_ids,
-            y: samples,
+            y: samplesData,
             mode: 'markers',
             marker: {
-                size: samples,
+                size: samplesData,
                 color: otu_ids,
                 colorscale: 'Y1GnBu'
 
@@ -69,8 +69,8 @@ function buildingTheCharts(sample) {
 
 
         let dataBarChart = [{
-            x: sample_values.slice(0,10).reverse(),
-            y: otu_ids.slice(0,10).reverse().map(otuID => 'OTU ${otuID}').reverse(),
+            x: samplesData.slice(0,10).reverse(),
+            y: otu_ids.slice(0,10).reverse().map(otuID => `OTU ${otuID}`).reverse(),
             text: otu_labels.slice(0,10).reverse(),
             type: 'bar',
             orientation: "h"
@@ -87,27 +87,29 @@ function buildingTheCharts(sample) {
 
     
     
-    
-    
-    
     }
+    
+    
+    )};
 
-}
+
+
+
 
 
 function buildingtheMeta(sample) {
-    d3.json(sample_data).then(function(data)) {
-        console.log("this is a metadata test")
-        console.log(data);
+    d3.json(testing_data).then(function(data) {
+        //console.log("this is a metadata test")
+        //console.log(data);
 
         let metadata = data.metadata;
-        console.log(metadata);
+        //console.log(metadata);
 
         let ArrayMetaData = metadata.filter(metadataObject => metadataObject.id == sample);
-        console.log(ArrayMetaData);
+        //console.log(ArrayMetaData);
 
         let ResultMeta = ArrayMetaData[0];
-        console.log(ResultMeta);
+        //console.log(ResultMeta);
 
 
         let metaPanel = d3.select("#sample-metadata");
@@ -115,10 +117,10 @@ function buildingtheMeta(sample) {
         metaPanel.html("");
 
         for (key in ResultMeta) {
-            metaPanel.append("h5").text('${key.toUpperCase()}: ${metadataResult[key]}');
+            metaPanel.append("h5").text(`${key.toUpperCase()}: ${ResultMeta[key]}`);
         };
     }
-}
+)};
 
 function optionHasChanged(newSample) {
     buildingTheCharts(newSample);
@@ -138,9 +140,9 @@ function optionHasChanged(newSample) {
 
 
 function initialize() {
-    d3.json(sample_data).then(function (data)) {
+    d3.json(testing_data).then(function (data) {
         let sampleNames = data.names;
-        console.log(sampleNames);
+        //console.log(sampleNames);
 
         let pullDown = d3.select("#selDataset");
 
@@ -154,8 +156,10 @@ function initialize() {
     
 
     let theFirstSample = sampleNames[0];
-    }
+    
     buildingTheCharts(theFirstSample);
     buildingtheMeta(theFirstSample);
+}
+)};
 
-};
+initialize();
